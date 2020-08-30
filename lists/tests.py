ff99c3e01ef30808331b949f64c0947e3df4099f
lc <liuchuang199831@gmail.com>
2020-08-30 18:13:11 +0800

    first test case for home pass

--- lists/tests.py
+++ lists/tests.py
@@ -6,4 +6,4 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        self.assertEqual(found.funcm, home_page)
+        self.assertEqual(found.func, home_page)
