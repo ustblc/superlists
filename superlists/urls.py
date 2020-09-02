967f32e50f28c54bb392b180b82dc7794b754f6c
lc <liuchuang199831@gmail.com>
2020-09-02 20:33:24 +0800

    add add_item and all test pass

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from lists import views
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.home_page, name="home_page"),
-    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
-    path("lists/new", views.new_list, name="new_list")
+    url(r"^$", views.home_page, name="home"),
+    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
