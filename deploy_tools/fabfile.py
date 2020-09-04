7066718b392698be2ad014b9e4e59500277c7df8
lc <liuchuang199831@gmail.com>
2020-09-04 20:40:24 +0800

    modify fab file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -27,7 +27,7 @@ def _get_latest_source(source_folder):
     else:
         run(f"git clone {REPO_URL}")
 
-    current_commit = local("git log -n -1 --format=%H", capture=True)
+    current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f"cd {source_folder} && git reset --hard {current_commit}")
 
 
