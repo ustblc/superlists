9556bdf4c28b0a5f3d77e7d3e3dc3edcd7599bd6
lc <liuchuang199831@gmail.com>
2020-09-04 20:56:04 +0800

    modify _get_latest_source

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -25,7 +25,7 @@ def _get_latest_source(source_folder):
     if exists(source_folder + "/.git"):
         run(f"cd {source_folder} && git fetch")
     else:
-        run(f"git clone {REPO_URL}")
+        run(f"git clone {REPO_URL} {source_folder}")
 
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f"cd {source_folder} && git reset --hard {current_commit}")
